8de4121c561bef3e9ffa4c6f39044013606d33bf
cotet <tcote9194@gmail.com>
2019-01-16 18:40:38 -0800

    Adding websocket JS

--- js/foodbuddy.js
+++ js/foodbuddy.js
@@ -1,3 +1,5 @@
+//Append recipes example
+
 $.getJSON('/json/example.json',
 function (data) {
     var tr;
@@ -9,3 +11,49 @@ function (data) {
         $('table').append(tr);
     }
 });
+
+// Websocket JS
+
+var foodbuddySocket = new WebSocket("ws://www.example.com/socketserver", "protocolOne");
+
+foodbuddySocket.onmessage = function (event) {
+  console.log(event.data);
+}
+
+exampleSocket.onmessage = function(event) {
+  var f = document.getElementById("chatbox").contentDocument;
+  var item = "";
+  var weight = JSON.parse(event.data);
+  var time = new Date(msg.date);
+  var timeStr = time.toLocaleTimeString();
+  
+  switch(msg.type) {
+    case "id":
+      clientID = msg.id;
+      setUsername();
+      break;
+    case "username":
+      text = "<b>User <em>" + msg.name + "</em> signed in at " + timeStr + "</b><br>";
+      break;
+    case "message":
+      text = "(" + timeStr + ") <b>" + msg.name + "</b>: " + msg.text + "<br>";
+      break;
+    case "rejectusername":
+      text = "<b>Your username has been set to <em>" + msg.name + "</em> because the name you chose is in use.</b><br>"
+      break;
+    case "userlist":
+      var ul = "";
+      for (i=0; i < msg.users.length; i++) {
+        ul += msg.users[i] + "<br>";
+      }
+      document.getElementById("userlistbox").innerHTML = ul;
+      break;
+  }
+  
+  if (text.length) {
+    f.write(text);
+    document.getElementById("chatbox").contentWindow.scrollByPages(1);
+  }
+};
+
+foodbuddySocket.close();
